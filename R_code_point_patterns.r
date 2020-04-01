0a26f8ca8daf1d5255f99b7e99326f37dbe5acd5
paso11doble <62288907+paso11doble@users.noreply.github.com>
2020-04-01 12:55:32 +0200

    Update R_code_point_patterns.r

--- R_code_point_patterns.r
+++ R_code_point_patterns.r
@@ -3,7 +3,7 @@
 install.packages("ggplot2") oppure library(ggplot2) [comando alternativo a library: require]
 install.packages("spatstat")
 
-# setwd("C:/lab/")
+# setwd("C:/lab/") per Windows
 
 # importare dati
 covid <- read.table("covid_agg.csv", head=T)
@@ -13,7 +13,7 @@ head(covid)
 plot(covid$country, covid$cases)
 il dollaro collega una colonna al dataset
 # attach(covid)
-# plot(country,cases)
+# plot(country, cases)
 
 plot(covid$country, covid$cases, las=0) #parallele agli assi
 plot(covid$country, covid$cases, las=1) #orizzontali
@@ -49,9 +49,40 @@ covids <- ppp(lon, lat, c(-180, 180), c(-90, 90))
 > ppp(x.coordinates, y.coordinates, x.range, y.range)
 
 d <- density(covids)
-plot(d)
-point(covids, pch=19)
+
 plot(d)
 points(covids)
 
+--------------------------------------------------------------------------------------------------------------
 
+# Save the .RData (file salvato nella cartella LAB del disco locale (C:))
+
+setwd("C:/LAB/")
+load("point_pattern.RData") (funzione per inserire l'RData di ieri)
+
+ls()
+
+plot(d)
+# palette (colori per rappresentare la mappa)
+cl <- colorRampPalette(c('yellow', 'orange', 'red')) (100)
+plot(d, col=cl)
+
+# Exercise: plot della mappa della densità dal verde al blu
+
+
+coastlines <- readOGR("ne_10m_coastline.shp") (devo utilizzare una funzione della libreria RGDAL, per leggere files vettoriali)
+
+quindi prima di dare il comando readOGR devo fare:
+install.packages("rgdal")
+library(rgdal)
+
+plot(coastlines, add=T) [inseriamo le coastlines e le aggiungiamo al plot precedente]
+
+points(covids) (funzione per inserire i centroidi di ieri)
+
+N.B. la mappa è relativa ai dati Covid di febbraio!
+
+# Exercise: plot della mappa della densità con una nuova colorazione e aggiunta delle coastlines
+cl <- colorRampPalette(c('blue', 'light blue', 'light green', 'yellow')) (100)
+plot(d, col=cl)
+plot(coastlines, add=T, col="green")
