4964de8541785cf98f14f43ae9bb989d0db2d45f
paso11doble <62288907+paso11doble@users.noreply.github.com>
2020-05-06 16:17:06 +0200

    Update R_code_multitemp.r

--- R_code_multitemp.r
+++ R_code_multitemp.r
@@ -1,38 +1,40 @@
 # R code: analisi multitemporale di variazione di copertura del suolo (Pratica del 29/04/20)
-   (utilizzo file deforestazione Foresta Amazzonica, Mato Grosso)
+   (file deforestazione Foresta Amazzonica, Mato Grosso, BR)
 
 setwd("C:/LAB/")
 
 library(raster)
 
-funzione "brick" per caricare dati dall'esterno
+funzione "brick": serve per caricare dati dall'esterno
 defor1 <- brick("defor1_.jpg")
 defor2 <- brick("defor2_.jpg")
 
-selezionando su R "defor1": troviamo al suo interno 3 bande
+selezionando "defor1": troviamo al suo interno 3 bande
 # names: defor1_.1, defor1_.2, defor1_.3 
-# defor1_.1 = nir
+# defor1_.1 = NIR
 # defor1_.2 = red
 # defor1_.3 = green
 
 # plot: associamo le 3 bande alle componenti RGB 
 plotRGB(defor1,r=1, g=2, b=3, stretch="Lin")
 
-# Exercise: plot della 2a data
+# Exercise: plot 2° data
 plotRGB(defor2,r=1, g=2, b=3, stretch="Lin")
 
-acqua pura assorbe completamente i.r. e diventa nera: 
-in una foto nell'acqua ci sono più solidi disciolti: 
-per questo motivo l'acqua risulta di due colori diversi
+confrontando le 2 foto, il fiume risulta di due colori diversi
+acqua pura assorbe completamente IR e diventa nera; questo significa che in una foto nel fiume ci sono più solidi disciolti 
 
+# plot delle 2 ortofoto
 par(mfrow=c(2,1))
 plotRGB(defor1,r=1, g=2, b=3, stretch="Lin")
 plotRGB(defor2,r=1, g=2, b=3, stretch="Lin")
  
-# classificazione non supervisionata (del defor1)
 
-funzione unsuperClass: per usarla dobbiamo caricare il pacchetto RStoolbox
+# classificazione non supervisionata (di defor1)
+
+funzione "unsuperClass": per usarla dobbiamo caricare il pacchetto RStoolbox
 library(RStoolbox)
+
 d1c <- unsuperClass(defor1, nClasses=2)
 dopo aver selezionato "d1c" faccio "d1c$map"
 
@@ -49,34 +51,29 @@ plot(d1c$map, col=cl)
 # plot(mappageologica$lito)
 # plot(mappageologica$lineaments)
 
-# classificazione (del defor2)
+# classificazione non supervisionata (di defor2)
 
 # Exercise: classificare con 2 classi l'immagine satellitare defor2
 d2c <- unsuperClass(defor2, nClasses=2)
-d2c$map
 plot(d2c$map)
-  cl <- colorRampPalette(c('green','black'))(100) #
-  plot(d2c$map, col=cl)
+cl <- colorRampPalette(c('black','green'))(100) #
+plot(d2c$map, col=cl)
   
-le 2 classi sono le foreste e tutto ciò che non è foresta
-
-dev.off()
-
 # plot delle 2 mappe ottenute
 par(mfrow=c(2,1))
 plot(d1c$map, col=cl)
 plot(d2c$map, col=cl)
-
+oppure
 par(mfrow=c(1,2))
 plot(d1c$map, col=cl)
 plot(d2c$map, col=cl)
 
-funzione "freq()" calcolare la frequenza dei valori
-freq(d1c$map)
+funzione "freq": serve per calcolare la frequenza dei valori
 
+freq(d1c$map)
 value  count
-# [1,]     1  33817  foresta
-# [2,]     2 307475  aree aperte
+# [1,]     1  33817  aree aperte
+# [2,]     2 307475  foresta
 
 totd1 <- 33817 + 307475
 
@@ -89,15 +86,18 @@ percent1 <- freq(d1c$map)  * 100/totd1
 
 percent1
             value     count
-[1,] 0.0002930042  9.908524  foresta
-[2,] 0.0005860085 90.091476  aree aperte
+[1,] 0.0002930042  9.908524  aree aperte
+[2,] 0.0005860085 90.091476  foresta
+
+# aree aperte: 9.9
+# foresta: 90.1
 
 facciamo lo stesso per la seconda mappa
 
- freq(d2c$map)
-     value  count
-# [1,]     1 163569 foresta
-# [2,]     2 179157 aree aperte
+freq(d2c$map)
+value  count
+# [1,]     1 163569 aree aperte
+# [2,]     2 179157 foresta
 
 totd2 <- 163569 + 179157
 
@@ -108,16 +108,18 @@ percent2 <- freq(d2c$map)  * 100/totd2
 
 percent2
             value    count
-[1,] 0.0002917783 47.72588 foresta
-[2,] 0.0005835565 52.27412 aree aperte
+[1,] 0.0002917783 47.72588 aree aperte
+[2,] 0.0005835565 52.27412 foresta
+
+# aree aperte: 47.7
+# foresta: 52.3
 
 cover <- c("Agriculture","Forest")
-before <- c(90,9.9)
-after <- c(52.3,47.7)
+before <- c(9.9, 90.1)
+after <- c(47.7, 52.3)
 
 output <- data.frame(cover,before,after)
 View(output)
-
 
 ----------------------------------------------------------------------------------------------------------------
 
@@ -131,7 +133,7 @@ library(raster)
 par(mfrow=c(1,2))
 cl <- colorRampPalette(c('black','green'))(100) #
 plot(d1c$map, col=cl)
-plot(d2c$map, col=cl)     (N.B. i colori in legenda potrebbero essere invertiti)
+plot(d2c$map, col=cl)
 
 output
        cover before after
@@ -141,7 +143,7 @@ output
 library(ggplot2)
 ggplot(output, aes(x=cover, y=before, color=cover)) + geom_bar(stat="identity", fill="white")
 
-x= copertura (agricoltura+foresta)
+x= copertura (agricoltura + foresta)
 y= % copertura prima della deforestazione
 per "identity": prendiamo direttamente i valori del colore (=copertura)
 
@@ -149,7 +151,7 @@ per "identity": prendiamo direttamente i valori del colore (=copertura)
 ggplot(output, aes(x=cover, y=after, color=cover)) + geom_bar(stat="identity", fill="white")
 
 install.packages("gridExtra") (N.B. "par" con ggplot non funziona!)
-require(gridExtra)
+library(gridExtra)
 
 # grid.arrange(plot1, plot2, nrow = 1)
 
@@ -178,20 +180,14 @@ output
 1 Agriculture   10.9  48.2
 2      Forest   89.1  51.8
 
-grafico1 <- ggplot(output, aes(x=cover, y=before, color=cover)) +
-+ geom_bar(stat="identity", fill="white")
-grafico2 <- ggplot(output, aes(x=cover, y=after, color=cover)) +
-+ geom_bar(stat="identity", fill="white")
+grafico1 <- ggplot(output, aes(x=cover, y=before, color=cover)) + geom_bar(stat="identity", fill="white")
+grafico2 <- ggplot(output, aes(x=cover, y=after, color=cover)) + geom_bar(stat="identity", fill="white")
   
 grid.arrange(grafico1, grafico2, nrow = 1)
 
 
-grafico1 <- ggplot(output, aes(x=cover, y=before, color=cover)) + 
-+ geom_bar(stat="identity", fill="white") +
-+ ylim(0, 100)
-grafico2 <- ggplot(output, aes(x=cover, y=after, color=cover)) + 
-+ geom_bar(stat="identity", fill="white") +
-+ ylim(0, 100)
+grafico1 <- ggplot(output, aes(x=cover, y=before, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(0, 100)
+grafico2 <- ggplot(output, aes(x=cover, y=after, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(0, 100)
 
-# Exercise: use grid.arrange to plot the two graphs 
-> grid.arrange(grafico1, grafico2, nrow = 1)
+# Exercise: use "grid.arrange" to plot the two graphs 
+grid.arrange(grafico1, grafico2, nrow = 1)
