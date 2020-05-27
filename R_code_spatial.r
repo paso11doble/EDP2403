0f8c1832a650fed833bb364fe0b7fad0799ed49b
paso11doble <62288907+paso11doble@users.noreply.github.com>
2020-05-27 10:24:55 +0200

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -15,7 +15,6 @@
 
 plot(cadmium, lead, col="red", pch=19, cex=2)
 
-
 # Exercise: plot di copper e zinc con simbolo triangolo (pch=17) e colore verde
 plot(copper, zinc, col="green", pch=17, cex=2)
 
@@ -44,7 +43,6 @@ head(meuse)
 coordinates(meuse)=~x+y
 plot(meuse)
 
-# per "plottare" i dati spazialmente: spplot
 spplot(meuse, "zinc")
 
 -----------------------------------------------------------------------------------------------------------
@@ -57,14 +55,14 @@ library(sp)
 data(meuse)
 head(meuse)
 
+# se voglio vedere i nomi delle colonne in alternativa a "head" posso usare: names(meuse)
+
 # come definire le coordinate del dataset: coordinates(meuse)=~x+y 
 
 # come plottare i dati nello spazio (in questo caso dello zinco): spplot(meuse, "zinc")
 
 # Exercise: fare spplot dei dati del rame
 spplot(meuse, "copper")
-
-# head(meuse); un'altra possibilità per vedere i nomi delle colonne è names(meuse)
 
 
 # un tipo di funzione per lavorare sui dati del plot: bubble
@@ -80,12 +78,11 @@ foram <- c(10, 20, 35, 55, 67, 80)
 carbon <- c(5, 15, 30, 70, 85, 99)
 
 # i dati sono collegati tra loro? sì sono in correlazione
-plot(foram, carbon,col="green", pch=19, cex=2)
-
+plot(foram, carbon, col="green", pch=19, cex=2)
 
 
 # DATI DALL'ESTERNO sul Covid-19
 
 # cartella da creare su Windows: Disco locale(C:)/LAB
 # percorso (path) da creare per Windows: setwd("C:/LAB") (*w.d. = working directory)
-# funzione per leggere la tabella (dall'esterno): covid <- read.table("covid_agg.csv", head=TRUE)
+# funzione per leggere la tabella: covid <- read.table("covid_agg.csv", head=TRUE)
