0a8c3baf6cb0ea6d3ec39a6c6bd1842162ef172a
paso11doble <62288907+paso11doble@users.noreply.github.com>
2020-05-28 16:13:37 +0200

    Update R_code_landcover.r

--- R_code_landcover.r
+++ R_code_landcover.r
@@ -1,32 +1,38 @@
-# R CODE LAND COVER (pratica del 21/04/20)
+# R CODE FOR LAND COVER: ANALISI DELLA COPERTURA DEL SUOLO (21/04/20)
 
 setwd("C:/LAB")
 
 library(raster)
-library(RStoolbox) (già installata)
+library(RStoolbox)
 
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 
-#rgb
+# rgb
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 
-unsuperClass() (con questo comando specifico immagine e n° classi) N.B. Classe in maiuscolo!
-                i pixel vengono accorpati nelle varie classi o cluster
+unsuperClass()
+# con questo comando devo specificare immagine e n° classi (indicate in Maiuscolo!)
+# i pixel vengono accorpati nelle varie classi o cluster
+               
 p224r63_2011c <- unsuperClass(p224r63_2011, nClasses=4)
 
-p224r63_2011c (con questo comando mi escono i dati dell'immagine)
+p224r63_2011c
+# in questo modo vengono elaborati i dati dell'immagine
 
-plot(p224r63_2011c$map) (per plottare la mappa)
+plot(p224r63_2011c$map)
+# faccio un plot della mappa col "$"
 
 clclass <- colorRampPalette(c('green', 'red', 'blue', 'black'))(100) 
 plot(p224r63_2011c$map, col=clclass)
-                         (per stabilire una legenda: ad ogni classe, 4, viene associato un colore
-                          i colori delle classi si notano nella scala sulla destra dell'immagine)
+# Legenda: ad ogni classe, 4 in tutto, viene associato un colore
+# i vari colori si notano nella scala sulla destra dell'immagine
                           
-p224r63_2011c <- unsuperClass(p224r63_2011, nClasses=2) (stesso processo con 2 classi)
-                         SENSITIVITA' dell'algoritmo all'aumentare del numero delle classi, all'aumentare dell'INCERTEZZA
-                         
-# IN FUNZIONE DEL N° CLASSI AUMENTA L'INCERTEZZA DELL'ALGORITMO AUTOMATICO DI CLASSIFICAZIONE
-# RIPORTANDO POTENZIALMENTE CLASSI LEGGERMENTE DIFFERENTI
-                         
+p224r63_2011c <- unsuperClass(p224r63_2011, nClasses=2) 
+# stesso processo con 2 classi
+  
 
+# CONSIDERAZIONI FINALI
+
+# RIPORTANDO POTENZIALMENTE CLASSI DIFFERENTI                      
+# L'INCERTEZZA DELL'ALGORITMO AUTOMATICO DI CLASSIFICAZIONE AUMENTA IN FUNZIONE DEL N° CLASSI: 
+# PER QUESTO SI PARLA DI SENSITIVITA' DELL'ALGORITMO
