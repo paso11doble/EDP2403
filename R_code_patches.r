630f180e5a36b87b005f128d3ca4b0758e398265
Michele Pasini <62288907+paso11doble@users.noreply.github.com>
2020-06-17 20:54:52 +0200

    Update R_code_patches.r

--- R_code_patches.r
+++ R_code_patches.r
@@ -16,13 +16,13 @@ cl <- colorRampPalette(c('green','black'))(100) #
 plot(d1c, col=cl)
 plot(d2c, col=cl)
 
-# 1° CASO: FORESTA E AGRICOLTURA
+# 1° CASO: AGRICOLTURA E FORESTA
 par(mfrow=c(1,2))
 cl <- colorRampPalette(c('black','green'))(100) # 
 plot(d1c, col=cl)
 plot(d2c, col=cl)
 
-# forest: class 2; agriculture: class 1
+# agriculture: class 1; forest: class 2 
 d1c.for <- reclassify(d1c, cbind(1,NA))
 d2c.for <- reclassify(d2c, cbind(1,NA))
 
@@ -40,7 +40,7 @@ plot(d1c)
 plot(d2c)
 
 
-FUNZIONE "clump": SERVE PER AGGREGARE LE PATCHES
+# FUNZIONE "clump": SERVE PER AGGREGARE LE PATCHES
 
 # creating patches
 install.packages("igraph")
@@ -52,11 +52,11 @@ d2c.for.patches <- clump(d2c.for)
 writeRaster(d1c.for.patches, "d1c.for.patches.tif")
 writeRaster(d2c.for.patches, "d2c.for.patches.tif")
 
-# FUNZIONE PER IMPORTARE DATI IN R -> "raster"
-# FUNZIONE PER ESPORTARE DATI DA R -> "writeRaster"
-
 d1c.for.patches <- raster("d1c.for.patches")
 d2c.for.patches <- raster("d2c.for.patches")
+
+# FUNZIONE PER IMPORTARE DATI IN R -> "raster"
+# FUNZIONE PER ESPORTARE DATI DA R -> "writeRaster"
 
 # Exercise: plottare entrambe le mappe una accanto all'altra 
 par(mfrow=c(1,2))
@@ -68,19 +68,38 @@ par(mfrow=c(1,2))
 plot(d1c.for.patches,col=clp)
 plot(d2c.for.patches,col=clp)
 
+d1c.for.patches
+# class      : RasterLayer 
+# dimensions : 478, 714, 341292  (nrow, ncol, ncell)
+# resolution : 1, 1  (x, y)
+# extent     : 0, 714, 0, 478  (xmin, xmax, ymin, ymax)
+# crs        : NA 
+# source     : memory
+# names      : clumps 
+# values     : 1, 301  (min, max)
+
+d2c.for.patches
+# class      : RasterLayer 
+# dimensions : 478, 717, 342726  (nrow, ncol, ncell)
+# resolution : 1, 1  (x, y)
+# extent     : 0, 717, 0, 478  (xmin, xmax, ymin, ymax)
+# crs        : NA 
+# source     : memory
+# names      : clumps 
+# values     : 1, 1212  (min, max)
+
 # max patches d1c.for.patches = 301
 # max patches d2c.for.patches = 1212
 
+
 # PLOT RESULTS
+
 time <- c("Before deforestation","After deforestation")
-npatches <- c(568,1920)
+npatches <- c(301,1212)
 
 output <- data.frame(time,npatches)
 attach(output)
 
-library(ggplot2)
+library(ggplot2) # for the final output plot
+
 ggplot(output, aes(x=time, y=npatches, color="red")) + geom_bar(stat="identity", fill="white")
-
-
-       
-funzione "crop": serve per fare un ritaglio su una determinata zona a partire da una scala globale
