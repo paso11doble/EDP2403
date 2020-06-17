2700a6223fc08ac8482fada06b056383f7e045f2
Michele Pasini <62288907+paso11doble@users.noreply.github.com>
2020-06-17 18:17:56 +0200

    Update R_code_landcover.r

--- R_code_landcover.r
+++ R_code_landcover.r
@@ -1,4 +1,5 @@
-# R CODE FOR LAND COVER: ANALISI DELLA COPERTURA DEL SUOLO (21/04/20)
+# R CODE FOR LAND COVER: 
+# ANALISI DELLA COPERTURA DEL SUOLO (21/04/20)
 
 setwd("C:/LAB")
 
@@ -11,29 +12,25 @@ p224r63_2011 <- brick("p224r63_2011_masked.grd")
 # PLOT RGB
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 
-# unsuperClass()
-# con questo comando, esclusivo di RStoolbox, devo specificare immagine e n° classi (in Maiuscolo!)
-# i pixel vengono accorpati nelle varie classi o cluster
+# "unsuperClass(...)": CON QUESTO COMANDO, PROPRIO DI RStoolbox, DEVO SPECIFICARE IMMAGINI E N° CLASSI (IN MAIUSCOLO!)
+# I PIXEL VENGONO ACCORPATI NELLE VARIE CLASSI/CLUSTER
                
 p224r63_2011c <- unsuperClass(p224r63_2011, nClasses=4)
 
 p224r63_2011c
-# in questo modo vengono elaborati i dati dell'immagine
 
 plot(p224r63_2011c$map)
-# faccio un plot della mappa col "$"
+# FACCIO UN PLOT DELLA MAPPA LEGATA ALL'IMMAGINE CON "$"
 
 clclass <- colorRampPalette(c('green', 'red', 'blue', 'black'))(100) 
 plot(p224r63_2011c$map, col=clclass)
-# Legenda: ad ogni classe, 4 in tutto, viene associato un colore
-# i vari colori si notano nella scala sulla destra dell'immagine
+# LEGENDA: AD OGNI CLASSE (4) VIENE ASSOCIATO UN COLORE, VISUALIZZATO NELLA SCALA ALLA DESTRA DELL'IMMAGINE
                           
 p224r63_2011c <- unsuperClass(p224r63_2011, nClasses=2) 
-# stesso processo con 2 classi
+# STESSO PROCESSO CON MENO CLASSI (2)
   
 
-# CONSIDERAZIONI FINALI
-
-# RIPORTANDO POTENZIALMENTE CLASSI DIFFERENTI                      
-# L'INCERTEZZA DELL'ALGORITMO AUTOMATICO DI CLASSIFICAZIONE AUMENTA IN FUNZIONE DEL N° CLASSI: 
+# CONSIDERAZIONI FINALI:
+                    
+# DATE CLASSI DIFFERENTI, L'INCERTEZZA DELL'ALGORITMO AUTOMATICO DI CLASSIFICAZIONE AUMENTA IN FUNZIONE DEL N° CLASSI: 
 # PER QUESTO SI PARLA DI SENSITIVITA' DELL'ALGORITMO
