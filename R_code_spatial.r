0b6a1329d781ddf698fde8b91fb32e32dcb972e1
Michele Pasini <62288907+paso11doble@users.noreply.github.com>
2020-06-17 16:36:15 +0200

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -1,15 +1,15 @@
 # R SPATIAL: CREAZIONE DI UN CODICE SPAZIALE (24/03/20)
 
-# BREVE REVIEW: 
-# funzione per installare pacchetti dall'esterno: install.packages("sp") (ricordarsi le virgolette!)
-# come richiamare il pacchetto (se già salvato): library(sp) / require(sp)
-# come richiamare i dati da usare: data(meuse)
-# come visualizzare l'incipit (prime 6 righe) della tabella: head(meuse)
+# BREVE REVIEW (ES. CON "MEUSE"): 
+# FUNZIONE PER INSTALLARE PACCHETTI DALL'ESTERNO: install.packages("sp") (ricordarsi le virgolette!)
+# COME RICHIAMARE UN PACCHETTO GIA' SALVATO: library(sp) / require(sp)
+# COME RICHIAMARE I DATI DA USARE: data(meuse)
+# COME VISUALIZZARE L'INCIPIT (PRIME 6 RIGHE) DELLA TABELLA: head(meuse)
 
 
 # plot cadmium e lead
 
-# come allegare il dataframe: attach(meuse)
+# COME ALLEGARE IL DATAFRAME: attach(meuse)
 
 plot(cadmium, lead, col="red", pch=19, cex=2)
 
@@ -18,26 +18,26 @@ plot(cadmium, lead, col="red", pch=19, cex=2)
 plot(copper, zinc, col="green", pch=17, cex=2)
 
 
-# come impostare le etichette ("X lable" e "Y lable") nel grafico
+# COME IMPOSTARE DELLE ETICHETTE/LABLES NEL GRAFICO
 plot(copper, zinc, col="green", pch=17, cex=2, xlab="rame", ylab="zinco")
 
 
-# multiframe o multipanel
+# MULTIFRAME O MULTIPANEL
 par(mfrow=c(1,2))
 plot(cadmium, lead, col="red", pch=19, cex=2)
 plot(copper, zinc, col="green", pch=17, cex=2) 
 
-# come invertire i plot riga/colonna in colonna/riga
+# COME INVERTIRE I PLOT RIGA/COLONNA IN COLONNA/RIGA
 par(mfrow=c(2,1))
 plot(cadmium, lead, col="red", pch=19, cex=2)
 plot(copper, zinc, col="green", pch=17, cex=2) 
 
 
-# multiframe automatico
+# MULTIFRAME AUTOMATICO
 install.packages("GGally")
 library(GGally)
 ggpairs(meuse[,3:6]) 
-# ("ggpairs" è una funzione esclusiva di GGally)
+# "ggpairs": E' UNA FUNZIONE DEL PACCHETTO GGally
 
 
 # Spatial!!
